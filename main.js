691f47be6aa14b8d262a2ce995376c7afbf2b96d
Robson Andrade <hagridd4@gmail.com>
2019-12-21 19:32:10 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-//Desestruturação
+//Operadores rest e spread
+
+//Rest
+/*
 const usuario = {
     nome: "Diego",
     idade: 23,
-    endereco: {
-     cidade:"Rio do sul",
-     estado:"SC",
-    },
+    empresa: "Rocketseat"
 };
-/*
-const nome = usuario.nome;
-const endereco = usuario.endereco.cidade;
-*/
-// No ES6 é possível usar outra sintaxe para acessar dados de uma classe
-
-/*
-const {nome,idade,endereco:{cidade}} = usuario;
+const {nome,...resto} = usuario;
 console.log(nome);
-console.log(idade);
-console.log(cidade);
+console.log(resto);
 */
-// É possível usar essa sintaxe numa function
-function mostranome({nome,idade}){
-    console.log(nome,idade);
+
+//Podemos aplicar isso também em vetores
+/*
+const arr = [1,2,3,4];
+const [a,b,...c] = arr;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Parâmetros de funções
+/*
+function soma(...params){
+    return params;
 }
-mostranome(usuario);
+console.log(soma(1,2,3,4,5));
+*/
+
+
+//Spread
+/*
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+const arr3 = [...arr1,...arr2];
+console.log(arr3);
+*/
+// Outra utilidade do spread
+const usuario = {
+     nome : "Diego",
+     idade : 23,
+     empresa: "Rocketseat"
+};
+const usuario2 = {...usuario, nome: "Gabriel"};
+console.log(usuario2);
+
+
+
+
+
+
+
