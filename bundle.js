f9208c4bbc23b6cab38c3bbdea5a15ae6c7444c0
Robson Andrade <hagridd4@gmail.com>
2019-12-21 19:37:46 -0300

    Template Literals

--- bundle.js
+++ bundle.js
@@ -1,58 +1,9 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+//Templates Literals
+// Esse é um jeito muito grande de concatenação.
+var nome = "Robson";
+var idade = 19;
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos "); // Esse é um jeito mais simples
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-//Operadores rest e spread
-//Rest
-
-/*
-const usuario = {
-    nome: "Diego",
-    idade: 23,
-    empresa: "Rocketseat"
-};
-const {nome,...resto} = usuario;
-console.log(nome);
-console.log(resto);
-*/
-//Podemos aplicar isso também em vetores
-
-/*
-const arr = [1,2,3,4];
-const [a,b,...c] = arr;
-console.log(a);
-console.log(b);
-console.log(c);
-*/
-//Parâmetros de funções
-
-/*
-function soma(...params){
-    return params;
-}
-console.log(soma(1,2,3,4,5));
-*/
-//Spread
-
-/*
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-const arr3 = [...arr1,...arr2];
-console.log(arr3);
-*/
-// Outra utilidade do spread
-var usuario = {
-  nome: "Diego",
-  idade: 23,
-  empresa: "Rocketseat"
-};
-
-var usuario2 = _objectSpread({}, usuario, {
-  nome: "Gabriel"
-});
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e eu tenho ").concat(idade, " anos."));
--- main.js
+++ main.js
@@ -1,53 +1,9 @@
-//Operadores rest e spread
+//Templates Literals
+// Esse é um jeito muito grande de concatenação.
+const nome = "Robson";
+const idade = 19;
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos ");
 
-//Rest
-/*
-const usuario = {
-    nome: "Diego",
-    idade: 23,
-    empresa: "Rocketseat"
-};
-const {nome,...resto} = usuario;
-console.log(nome);
-console.log(resto);
-*/
+// Esse é um jeito mais simples
 
-//Podemos aplicar isso também em vetores
-/*
-const arr = [1,2,3,4];
-const [a,b,...c] = arr;
-console.log(a);
-console.log(b);
-console.log(c);
-*/
-//Parâmetros de funções
-/*
-function soma(...params){
-    return params;
-}
-console.log(soma(1,2,3,4,5));
-*/
-
-
-//Spread
-/*
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-const arr3 = [...arr1,...arr2];
-console.log(arr3);
-*/
-// Outra utilidade do spread
-const usuario = {
-     nome : "Diego",
-     idade : 23,
-     empresa: "Rocketseat"
-};
-const usuario2 = {...usuario, nome: "Gabriel"};
-console.log(usuario2);
-
-
-
-
-
-
-
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);
